bc9392e0a1d8348887bcc199b13d0cae740eca6a
Davlat <davlatberdinazarov0708@gmail.com>
2024-04-14 08:52:51 +0500

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import AppLayout from './layouts/AppLayout';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import Profile from './pages/Profile';
+import Favorites from './pages/Favorites';
+import MyCart from './pages/MyCart';
+import Mens from './pages/Mens';
+import Womens from './pages/Womens';
+import Kids from './pages/Kids';
+import ProductDetailPage from './pages/ProductDetailPage';
+import NotFound from './pages/NotFound';
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<AppLayout />}
+        >
+          <Route path='/login' element={<Login/>} />
+          <Route
+            path="/"
+            element={<MainLayout />}
+          >
+            <Route index element={<HomePage/>} />
+            <Route path="profile" element={<Profile/>} />
+            <Route path="favorites" element={<Favorites />} />
+            <Route path="cart" element={<MyCart />} />
+            <Route path="mens" element={<Mens />} />
+            <Route path="womens" element={<Womens />} />
+            <Route path="kids" element={<Kids />} />
+            <Route path="product/:id" element={<ProductDetailPage />} />
+          </Route>
+          <Route path="*" element={<NotFound to="/" />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
